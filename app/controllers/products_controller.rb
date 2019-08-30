c8225381d0e41b725dae31114b667e22aa3c5763
Jonathan Luk <jluk87@gmail.com>
2019-08-30 16:19:55 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,7 +23,7 @@ class ProductsController < ApplicationController
   end
 
   def inventory
-    render plain: @product.inventory > 0 ? 'true' : 'false'
+    render plain: @product.inventory > 0 ? true : false
   end
 
   private
