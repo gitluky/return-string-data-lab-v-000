966717a0505f112a39b1a57c3368c2ead250c4d9
Jonathan Luk <jluk87@gmail.com>
2019-08-30 16:15:09 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :invoices
   resources :products, only: [:index, :new, :create]
   get 'products/:id/description', to: 'products#description'
-  get 'products/:id/availability', to: 'products#availability'
+  get 'products/:id/inventory', to: 'products#inventory'
 end
